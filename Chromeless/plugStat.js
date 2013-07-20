b889b969b4b30475ac8af95a81ad4ef52d369027
Bftech <corentin.monmasson@gmail.com>
2013-07-20 21:37:59 +0200

    Create plugStat.js

---
+++ Chromeless/plugStat.js
@@ -0,0 +1,22 @@
+API.on(API.DJ_ADVANCE, stat);
+function stat(obj)
+{
+if (obj == null) return; // no dj
+ 
+  var lastPlay = obj.lastPlay;
+  var lastVid = lastPlay.media;
+  var lastStat = lastPlay.score;
+  var msg = '';
+  msg += lastVid.author;
+  msg += ' - ';
+  msg += lastVid.title;
+  msg += ' : ';
+  msg += lastStat.positive;
+  msg += ' Woots | ';
+  msg += lastStat.negative;
+  msg += ' Mehs | ';
+  msg += lastStat.curates;
+  msg += ' Curates';
+  
+  API.sendChat(msg);
+}
